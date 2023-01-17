36f45d357013b561bba96cc8aa2bc12aa4c1f75a
HeeHyun8102 <64294967+HeeHyun8102@users.noreply.github.com>
2023-01-17 19:21:22 +0900

    Update programmers_PriorityQueue.py

--- algorithm/김희현/week3_HeepGraph/programmers_PriorityQueue.py
+++ algorithm/김희현/week3_HeepGraph/programmers_PriorityQueue.py
@@ -4,20 +4,25 @@ def solution(operations):
     answer = []
     heap = []   # 힙 생성
 
-    for data in operations:
+    for o in operations:
         # 연산이 "I"일 경우 공백 뒤의 숫자를 heap에 넣음
-        if data[0] == "I":
-            heapq.heappush(heap, int(data[2:]))
+        if o[0] == "I":
+            heapq.heappush(heap, int(o[2:]))
+        
         # 연산이 "D"일 경우
         else:
+            #  빈 큐일 경우 패스
             if len(heap) == 0:
                 pass
+            
             # 공백 뒤가 "-"일 경우 최소힙을 제거
-            elif data[2] == "-":
+            elif o[2] == "-":
                 heapq.heappop(heap)
+                
             # 공백 뒤가 아무것도 아닌 경우
             else:
                 # 힙에서 가장 큰 수를 제외하고 다시 힙에 넣음
+                # nlargest : n개의 큰 값을 힙으로 생성
                 heap = heapq.nlargest(len(heap), heap)[1:]
                 heapq.heapify(heap)
 
